e8763b39748ce2f6aa87db198f227d3508708698
Anil Patidar <patidaranil0791@gmail.com>
2019-10-15 04:27:20 +0530

    Create Simple Graph.py

---
+++ Matplotlib/Simple Graph/Simple Graph.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('bmh')
+
+plt.plot([1,2,3],[44,20,30],[100,150,200])  # 1->44, 2->20, 3->30   #1->8, 2->5, 3->6
+
+plt.show()  # for 3 value(x,y,y)(2 graph)  for 4 value(x,y,x,y)(2 graph)
